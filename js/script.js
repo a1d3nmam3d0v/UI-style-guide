d884fb7aec45e72dc682b52f4217038acacf3cf3
aiden <aiden@aiden.com>
2023-06-16 11:19:26 -0500

    Add breakpoint to remove margins on mobile screens

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-let sections = document.querySelectorAll('section');
-let links = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let links = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach((sec) => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top <= offset + height) {
-            links.forEach((link) => {
-                link.classList.remove('active');
-                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-            });
-        }
-    });
+    if (top >= offset && top <= offset + height) {
+      links.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector('header nav a[href*="' + id + '"]')
+          .classList.add("active");
+      });
+    }
+  });
 };
